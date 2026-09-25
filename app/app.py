f1705d2c8f3a849fc82343cbcfe0919d79d9f8da
Kiharu07 <sak1001haruki@gmail.com>
2026-09-25 01:33:25 +0000

    基本的な大挙動を実装途中

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
+import function as func
+import random
+
 #大挙動
 """
 初期設定
@@ -16,4 +19,42 @@
 取引→フラッグ
 ログに保存
 """
+#定数置き場：開発途中でconfig.yamlに移します。
+STREAM_LENGTH = 10 #streamの長さ
+SELL_BORDER = 114514 #sell_pointと比べて売判断する閾値
+BUY_BORDER = 114514 #buy_pointと比べて買判断する閾値
+
+
+#初期設定
+data = {
+    'stream': [],#データストリーム
+    'status': 'idle',#sell,idleで保持状態を表記
+}
+
+
+while True:
+    #データを取得（後にyfinanceで取得）
+    now_price = func.dummy_price01()
+    
+    #データを格納
+    data['stream'].append(now_price)
+    
+    #データリストが特定以上になったら古い順に削除。
+    if len(data['stream']) > STREAM_LENGTH:
+        data['stream'].pop(0)
+    
+    #売買点を計算
+    sell_point = random.randint[0,100]
+    buy_point = random.randint[0,100]
+    
+
+    #売買判断を閾値で判定
+    if data['status']=='idle' and sell_point > SELL_BORDER:
+        print('売り注文。')
+        data['status'] = 'sell'
+    elif data['status']=='sell' and buy_point < BUY_BORDER:
+        print('取引完了。')
+    #取引→フラッグ
+    #ログに保存
+    print(data['stream'])
 
--- app/function.py
+++ app/function.py
@@ -1,1 +1,13 @@
+import random
 
+"""ダミーデータ（開発用）"""
+def dummy_price01()->float:
+    """
+    dummy_の値段をfloatで返す。
+    完全な乱数なので傾向などは無い。
+    """
+    amplitude = 0.1*random.randint[-100,100]#変化幅(-10<x<10)
+    val = 140 + amplitude 
+    return val
+
+"""売買点関係"""
