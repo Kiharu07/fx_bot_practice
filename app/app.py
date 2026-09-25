6a97e1e2b481d55bb9e0f7cb12c81a222ead97c4
Kiharu07 <sak1001haruki@gmail.com>
2026-09-25 03:15:16 +0000

    ダミーデータ版完成

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import function as func
 import random
+import time
 
 #大挙動
 """
@@ -21,14 +22,17 @@ import random
 """
 #定数置き場：開発途中でconfig.yamlに移します。
 STREAM_LENGTH = 10 #streamの長さ
-SELL_BORDER = 114514 #sell_pointと比べて売判断する閾値
-BUY_BORDER = 114514 #buy_pointと比べて買判断する閾値
+SELL_BORDER = 70 #sell_pointと比べて売判断する閾値
+BUY_BORDER = 70 #buy_pointと比べて買判断する閾値
+
 
 
 #初期設定
 data = {
     'stream': [],#データストリーム
     'status': 'idle',#sell,idleで保持状態を表記
+    'sell_point': 0,
+    'buy_point': 0,
 }
 
 
@@ -38,23 +42,25 @@ while True:
     
     #データを格納
     data['stream'].append(now_price)
-    
+
+    if len(data['stream']) < STREAM_LENGTH:
+        print('データ準備中')
     #データリストが特定以上になったら古い順に削除。
-    if len(data['stream']) > STREAM_LENGTH:
+    else:
         data['stream'].pop(0)
-    
-    #売買点を計算
-    sell_point = random.randint[0,100]
-    buy_point = random.randint[0,100]
-    
 
-    #売買判断を閾値で判定
-    if data['status']=='idle' and sell_point > SELL_BORDER:
-        print('売り注文。')
-        data['status'] = 'sell'
-    elif data['status']=='sell' and buy_point < BUY_BORDER:
-        print('取引完了。')
-    #取引→フラッグ
+        #売買点を計算（今はまだ）
+        data['sell_point'] = random.randint(0,100)
+        data['buy_point'] = random.randint(0,100)
+
+        #売買判断を閾値で判定→取引→フラッグ
+        if data['status']=='idle' and data['sell_point'] > SELL_BORDER:
+            print('売り注文。')
+            data['status'] = 'sell'
+        elif data['status']=='sell' and data['buy_point'] < BUY_BORDER:
+            print('取引完了。')
+            data['status'] = 'idle'
+    
     #ログに保存
-    print(data['stream'])
-
+    print(f"リスト:{data['stream']},sell:{data['sell_point']},buy:{data['buy_point']},status:{data['status']}")
+    time.sleep(0.5)
--- app/function.py
+++ app/function.py
@@ -6,7 +6,7 @@ def dummy_price01()->float:
     dummy_の値段をfloatで返す。
     完全な乱数なので傾向などは無い。
     """
-    amplitude = 0.1*random.randint[-100,100]#変化幅(-10<x<10)
+    amplitude = 0.1*random.randint(-100,100)#変化幅(-10<x<10)
     val = 140 + amplitude 
     return val
 
